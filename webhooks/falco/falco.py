d5b6624da38cf4ec34306f31e5bf0e9d42e3770a
Juan Matias Kungfoo de la Camara Beovide <juan.delacamara@binbash.com.ar>
2024-07-08 15:32:01 -0300

    Added falco base webhook

---
+++ webhooks/falco/falco.py
@@ -0,0 +1,43 @@
+from flask import current_app
+
+from alerta.app import alarm_model
+from alerta.models.alert import Alert
+
+from . import WebhookBase
+
+class FalcoWebhook(WebhookBase):
+    """
+    Falco webhook
+    """
+
+    def incoming(self, path, query_string, payload):
+
+        if payload['importance_level'] == 'HIGH':
+            severity = 'critical'
+        else:
+            severity = 'warning'
+
+        if payload['current_state'] == 'UP':
+            severity = alarm_model.DEFAULT_NORMAL_SEVERITY
+
+        return Alert(
+            origin='Falco',
+            event_type='availabilityAlert',
+            raw_data=payload
+            )
+        #return Alert(
+        #    resource=payload['check_name'],
+        #    event=payload['current_state'],
+        #    correlate=['UP', 'DOWN'],
+        #    environment=current_app.config['DEFAULT_ENVIRONMENT'],
+        #    severity=severity,
+        #    service=[payload['check_type']],
+        #    group='Network',
+        #    value=payload['description'],
+        #    text=f"{payload['importance_level']}: {payload['long_description']}",
+        #    tags=payload['tags'],
+        #    attributes={'checkId': payload['check_id']},
+        #    origin='Falco',
+        #    event_type='availabilityAlert',
+        #    raw_data=payload
+        #)
